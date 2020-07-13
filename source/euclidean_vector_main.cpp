632279ed28c423596cf10501311c9202e170eb08
z5216888 <z5216888@cse.unsw.edu.au>
2020-07-13 19:56:20 +1000

    main file changes

--- source/euclidean_vector_main.cpp
+++ source/euclidean_vector_main.cpp
@@ -17,37 +17,17 @@ auto main() -> int {
 
 	auto f = comp6771::euclidean_vector{2, 4.0, 6.0, 8.0};
 
-	// NOLINTNEXTLINE(performance-unnecessary-copy-initialization)
-	auto g = a;
-
-	// e[1] = -4.0;
-	std::cout << a << "\n";
-	// std::cout << b << "\n";
-	// std::cout << c << "\n";
-	std::cout << d << "\n";
-	// std::cout << e << "\n";
-	// std::cout << f << "\n";
-	std::cout << g << "\n";
-	g *= 2;
-	std::cout << g << "\n";
-	g /= 6;
-	std::cout << g << "\n";
-	g = a / 3;
-	g -= a;
-	std::cout << g << "\n";
-
-	// auto h = static_cast<std::list<double>>(d);
-	// for (auto const i : h) {
-	// 	std::cout << i << " ";
-	// }
-
 	std::cout << a.at(0) << "\n";
 
+	std::cout << a << "\n";
+	std::cout << b << "\n";
+	std::cout << c << "\n";
+	std::cout << d << "\n";
+	std::cout << e << "\n";
+	std::cout << f << "\n";
+
 	auto const i = comp6771::euclidean_vector{1, 2, 3};
-	// auto const j = comp6771::euclidean_vector{1, 2};
-	// auto const k = comp6771::euclidean_vector{3, 4};
 	std::cout << comp6771::euclidean_norm(i) << "\n";
-	// std::cout << comp6771::dot(j, k) << "\n";
 	std::cout << comp6771::unit(i) << "\n";
 
 	return 1;
